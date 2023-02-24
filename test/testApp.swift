b1fc764b5bd03c95ec3a2b89d73fc66ced70c244
ahmadis00 <ahmad.nismail9@gmail.com>
2023-02-24 15:23:53 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by mac on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
